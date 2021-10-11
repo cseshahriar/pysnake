611a5d0986148641f1edde2ac4cfcf9bf2c898fc
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-10-12 00:39:29 +0600

    make oop with surface and block

--- main.py
+++ main.py
@@ -2,52 +2,74 @@ import pygame
 from pygame.locals import *
 
 
-def draw_block():
-    """ draw block """
-    surface.fill((110, 110, 5))  # clear the state
-    surface.blit(block, (block_x, block_y))
-    pygame.display.flip()
+class Snake:
+    """ snake """
+
+    def __init__(self, parent_screen):
+        self.parent_screen = parent_screen
+        self.block = pygame.image.load("resources/block.jpg").convert()
+        self.x = 100
+        self.y = 100
+
+    def draw(self):
+        self.parent_screen.fill((110, 110, 5))  # clear the state
+        self.parent_screen.blit(self.block, (self.x, self.y))
+        pygame.display.flip()
+
+    def move_left(self):
+        self.x -= 10
+        self.draw()
+
+    def mode_right(self):
+        self.x += 10
+        self.draw()
+
+    def move_up(self):
+        self.y -= 10
+        self.draw()
+
+    def mode_down(self):
+        self.y += 10
+        self.draw()
+
+
+class Game:
+    """ game class """
+
+    def __init__(self):
+        pygame.init()  # py game initialize
+        self.surface = pygame.display.set_mode((500, 500))
+        self.surface.fill((110, 110, 5))  # go rgb color picker
+        self.snake = Snake(self.surface)
+        self.snake.draw()
+
+    def run(self):
+        running = True
+
+        while running:
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+
+                    if event.key == K_ESCAPE:
+                        running = False
+
+                    if event.key == K_LEFT:
+                        self.snake.move_left()
+
+                    if event.key == K_UP:
+                        self.snake.move_up()
+
+                    if event.key == K_DOWN:
+                        self.snake.mode_down()
+
+                    if event.key == K_RIGHT:
+                        self.snake.mode_right()
+
+                elif event.type == QUIT:
+                    running = False  # exit
 
 
 if __name__ == "__main__":
-    pygame.init()  # py game initialize
-
-    surface = pygame.display.set_mode((500, 500))
-    surface.fill((110, 110, 5))  # go rgb color picker
-    pygame.display.flip()  # update
-
-    # block
-    block = pygame.image.load("resources/block.jpg").convert()
-    block_x = 100
-    block_y = 100
-    surface.blit(block, (block_x, block_y))
-    pygame.display.flip()
-
-    running = True
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == KEYDOWN:
-
-                if event.key == K_ESCAPE:
-                    running = False
-
-                if event.key == K_UP:
-                    block_y -= 10
-                    draw_block()
-
-                if event.key == K_DOWN:
-                    block_y += 10
-                    draw_block()
-
-                if event.key == K_LEFT:
-                    block_x -= 10
-                    draw_block()
-
-                if event.key == K_RIGHT:
-                    block_x += 10
-                    draw_block()
-
-            elif event.type == QUIT:
-                running = False  # exit
+    game = Game()
+    game.run()
 
